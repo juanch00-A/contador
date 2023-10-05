7de563d24f75e158f71acd539e581cdd9301ee3b
Juan Albarran <132104241+juanch00-A@users.noreply.github.com>
2023-10-05 15:23:52 -0400

    contador-react

---
+++ src/HooksApp.jsx
@@ -0,0 +1,13 @@
+import { ContadorComponent } from "./components/ContadorComponent"
+
+ContadorComponent
+export const HooksApp = () => {
+    return (
+        <>
+        <h1>Aplicacion de Hooks</h1>
+        <hr />
+        <ContadorComponent></ContadorComponent>
+        </>
+        
+    )
+}
